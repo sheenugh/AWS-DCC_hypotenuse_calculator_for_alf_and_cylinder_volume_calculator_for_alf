26d347f588f857cc9b92d5fe244e35de88dced8d
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 21:48:28 +0800

    Pseudo Code

---
+++ hypotenuse_calculator_for_alf.py
@@ -0,0 +1,9 @@
+# ========== IMPORT/S ==========
+
+
+# ========== PSEUDO CODE ==========
+# - Printing the 'HYPOTENUSE CALCULATOR FOR ALF"
+# - Asking user for the value of the two legs of the right triangle
+# - Calculation of the right triangle's hypotenuse using Pythagorean Theorem
+# - Display the calculated hypotenuse
+
