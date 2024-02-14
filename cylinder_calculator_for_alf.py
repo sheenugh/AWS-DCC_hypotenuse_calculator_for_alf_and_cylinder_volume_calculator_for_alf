2174a3a9307d446168eff4e5cf86fbf4fc701d24
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 23:04:43 +0800

    done

--- cylinder_calculator_for_alf.py
+++ cylinder_calculator_for_alf.py
@@ -1,9 +1,23 @@
 
 # ========== IMPORT/S ==========
+import math
+
+# ========== VARIABLE/S==========
 
 
 # ========== PSEUDO CODE ==========
 # - Printing 'CYLINDER VOLUME CALCULATOR FOR ALF'
+print("CYLINDER VOLUME CALCULATOR FOR ALF")
+
+
 # - Asking the user for his/her desire value for the radius and height of the cylinder
+radius_value_from_the_user = float(input("Enter a number for the value of radius of the cylinder:"))
+height_value_from_the_user = float(input("Enter a number for the value of height of the cylinder:"))
+
+
 # - Calculating the volume of the cylinder using the formula V = πr^2h.
+volume = math.pi*pow(radius_value_from_the_user,2)*height_value_from_the_user
+
+
 # - Displaying the the calculated volume with 2 decimal places.
+print("The volume of the cylinder is", "%.2f" % volume)
