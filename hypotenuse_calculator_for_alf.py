6fe488fc92ef92a6101602134027fb65ee608d32
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 21:55:59 +0800

    done

--- hypotenuse_calculator_for_alf.py
+++ hypotenuse_calculator_for_alf.py
@@ -1,9 +1,16 @@
 # ========== IMPORT/S ==========
-
+from math import sqrt
 
 # ========== PSEUDO CODE ==========
 # - Printing the 'HYPOTENUSE CALCULATOR FOR ALF"
-# - Asking user for the value of the two legs of the right triangle
-# - Calculation of the right triangle's hypotenuse using Pythagorean Theorem
-# - Display the calculated hypotenuse
+print("HYPOTENUSE CALCULATOR FOR ALF")
 
+# - Asking user for the value of the two legs of the right triangle
+opposite_side = float(input("Enter a number for the length of opposite side:"))
+adjacent_side = float(input("Enter a number for the length of adjacent side:"))
+
+# - Calculation of the right triangle's hypotenuse using Pythagorean Theorem
+hypotenuse_side_result = sqrt(opposite_side**2 + adjacent_side**2)
+
+# - Display the calculated hypotenuse
+print("The hypotenuse of the right triangle is", "%.2f" % hypotenuse_side_result)
